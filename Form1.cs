1e8ae7a0cef25b6a33ffe371f9395b91f2b0209a
Артём Чернов <artem_chernow@mail.ru>
2021-10-12 18:35:03 +0800

    Добавил комбобокс и поменял немного форму

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace laba_3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
